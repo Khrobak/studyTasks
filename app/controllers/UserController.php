3eb1bb0addc1492ae5be37f61276c5cf8282a414
Khrobak <mariyapanaeva512@gmail.com>
2024-06-30 15:56:53 +0300

    add counter

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -27,7 +27,52 @@ class UserController extends Controller
 
     public function index()
     {
+        var_dump(session_id());
+//выделяем уникальный идентификатор сессии
+        $id = session_id();
+
+        if ($id!=="") {
+            //текущее время
+            $CurrentTime = time();
+            //через какое время сессии удаляются
+            $LastTime = time() - 60;
+            //файл, в котором храним идентификаторы и время
+            $base = "session.txt";
+
+            $file = file($base);
+            $k = 0;
+            $ResFile = [];
+            for ($i = 0; $i < sizeof($file); $i++) {
+                $line = explode("|", $file[$i]);
+                if ($line[1] > $LastTime) {
+                    $ResFile[$k] = $file[$i];
+                    $k++;
+                }
+            }
+            $is_sid_in_file=0;
+
+            for ($i = 0; $i<sizeof($ResFile); $i++) {
+                $line = explode("|", $ResFile[$i]);
+                if ($line[0]==$id) {
+                    $line[1] = trim($CurrentTime)."\n";
+                    $is_sid_in_file = 1;
+                }
+                $line = implode("|", $line); $ResFile[$i] = $line;
+            }
+
+            $fp = fopen($base, "w");
+            for ($i = 0; $i<sizeof($ResFile); $i++) { fputs($fp, $ResFile[$i]); }
+            fclose($fp);
+
+            if (!$is_sid_in_file) {
+                $fp = fopen($base, "a-");
+                $line = $id."|".$CurrentTime."\n";
+                fputs($fp, $line);
+                fclose($fp);
+            }
+        }
         return $this->view->generate('index.twig');
+
     }
 
     public function create()
